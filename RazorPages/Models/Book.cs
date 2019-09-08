fdd41bb46c4ad10401fc50d9e43f18b7633a59dd
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2019-09-08 16:03:26 -0400

    implement razor pages with client side validation using unobtrusive jquery validate

---
+++ RazorPages/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPages.Models
+{
+    public class Book
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public int Pages { get; set; }
+        public string Genre { get; set; }
+        
+    }
+}
---
+++ RazorPages/Pages/AddBook.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RazorPages.Models;
+
+namespace RazorPages.Pages
+{
+    public class AddBookModel : PageModel
+    {
+        private readonly ILogger<AddBookModel> _logger;
+        
+        public AddBookModel(ILogger<AddBookModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData.Add("Success", true);
+            }
+            return Page();
+        }
+    }
+}
